92662bf0a6ad4ff9489ea37c60f5a0d0438b41c1
snir2912 <89028623+snir2912@users.noreply.github.com>
2022-10-20 19:07:34 +0300

    blocking user by using profane words.

--- backend/controller/posts/Posts-controller.js
+++ backend/controller/posts/Posts-controller.js
@@ -1,9 +1,21 @@
 const expressAsyncHandler = require("express-async-handler");
 const Post = require("../../models/post-model/Post-model");
 const validateMongodbId = require("../../utils/validateMongodbId");
+const Filter = require("bad-words");
+const User = require("../../models/user-model/User-model");
 
 const createPostCtrl = expressAsyncHandler(async (req, res) => {
+  const { _id } = req.user;
   validateMongodbId(req.body.user);
+  const filter = new Filter();
+  const isProfane = filter.isProfane(req.body.title, req.body.description);
+  if (isProfane) {
+    await User.findByIdAndUpdate(_id, {
+      isBlocked: true,
+    });
+    throw new Error("התמשת במילים לא הולמות, המשתמש שלך נחסם");
+  }
+
   try {
     const post = await Post.create(req.body);
     res.json(post);
