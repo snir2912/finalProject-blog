3b83ccf4a084c0f0c1834560c3775fa21d1a50ee
snir2912 <89028623+snir2912@users.noreply.github.com>
2022-10-29 10:56:22 +0300

    get all comments feature.

--- backend/routes/comments/Comment-route.js
+++ backend/routes/comments/Comment-route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const {
   createComment,
+  getAllComments,
 } = require("../../controller/comments/comment-controller");
 const authMiddleware = require("../../middlewares/Auth/authMiddleware");
 
 const commentRoute = express.Router();
 
 commentRoute.post("/", authMiddleware, createComment);
+commentRoute.get("/", authMiddleware, getAllComments);
 
 module.exports = commentRoute;
