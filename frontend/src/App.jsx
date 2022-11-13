3782247387abbc26b481d4438946259aac383efd
snir2912 <89028623+snir2912@users.noreply.github.com>
2022-11-13 11:48:20 +0200

    login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import HomePage from "./components/HomePage/HomePage";
+import Login from "./components/Users/login/Login";
 import Register from "./components/Users/Register/Register";
+
 function App() {
   return (
     <BrowserRouter className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
       </Switch>
     </BrowserRouter>
   );
