6b7cd71a9f1e74d79557d241f6d1516f736c8bf8
snir2912 <89028623+snir2912@users.noreply.github.com>
2022-11-13 21:35:42 +0200

    create category form and add category to the db.

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../slices/users/usersSlices";
+import categoriesReducer from "../slices/category/categorySlice";
 const store = configureStore({
   reducer: {
     users: usersReducer,
+    category: categoriesReducer,
   },
 });
 
