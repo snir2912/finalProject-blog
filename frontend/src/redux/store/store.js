8ab12e0edd40a4e742997af07a4ee00900c7f709
snir2912 <89028623+snir2912@users.noreply.github.com>
2022-11-28 18:21:36 +0200

    create post form, action amd reducer.

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../slices/users/usersSlices";
 import categoriesReducer from "../slices/category/categorySlice";
+import post from "../slices/posts/PostSlices";
 const store = configureStore({
   reducer: {
     users: usersReducer,
     category: categoriesReducer,
+    post,
   },
 });
 
